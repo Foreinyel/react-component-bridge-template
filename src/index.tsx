fe270854a4168b4c21b9d137c2a32fc845450739
Foreinyel <Foreinyel@163.com>
2022-06-12 20:22:24 +0800

    react-component-bridge-template

---
+++ src/index.tsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useRef, useLayoutEffect, useState } from "react";
+const useCustomLayoutEffect =
+  typeof window !== "undefined" ? useLayoutEffect : useEffect;
+const load = (function () {
+  return System.import("{{ resourceUrl }}");
+})();
+
+export interface BridgeProps {}
+
+export const Bridge = (props: any) => {
+  const [loaded, setLoaded] = useState(false);
+  const ref = useRef();
+
+  useCustomLayoutEffect(() => {
+    load.then((mod: any) => {
+      ref.current = mod.default;
+      setLoaded(true);
+    });
+  }, []);
+
+  const Component = ref.current;
+
+  return loaded && Component ? <Component {...props} /> : null;
+};
+
+export default Bridge;
